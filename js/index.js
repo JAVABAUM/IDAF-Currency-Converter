07a92f8b6c46102ceda540bb9e424758eaf0c569
slaxeea <kappelers77@gmail.com>
2021-08-19 13:27:24 +0200

    js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
-var baseUrl = "https://free.currconv.com";
-var apiKey = getApi();
+function convertCurrency(from, to, amount) {
+  var baseUrl = "https://free.currconv.com";
+  var apiKey = getApi();
+
+  fromCurrency = encodeURIComponent(from);
+  toCurrency = encodeURIComponent(to);
+  var query = fromCurrency + "_" + toCurrency;
+
+  var url =
+    baseUrl + "api/v7/convert?q=" + query + "&compact=ultra&apiKey=" + apiKey;
+
+  $.ajax({
+    url: url,
+    context: document.body,
+  }).done(function () {
+      console.log("sucess");
+  });
+}
