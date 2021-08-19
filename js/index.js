1d32289a0aca9002fe28f306bdcf2090fdc27da2
slaxeea <kappelers77@gmail.com>
2021-08-19 14:06:00 +0200

    js 1.1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function convertCurrency(from, to, amount) {
   var apiKey = getApi();
 
-  var url = `https://freecurrencyapi.net/api/v1/rates?base_currency=${from}&apikey=cb4655c0-00e1-11ec-8704-774472d2bb87`;
+  var url = `https://freecurrencyapi.net/api/v1/rates?base_currency=${from}&apikey=${apiKey}`;
 
   var request = new XMLHttpRequest();
   request.open("GET", url, true);
@@ -9,10 +9,9 @@ function convertCurrency(from, to, amount) {
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(this.response);
-      // console.log(data);
       // console.log(data.data["2021-08-18"][to]);
-      console.log(calculateAmount(amount , data.data["2021-08-18"][to]))
-
+      var date = dateString();
+      console.log(calculateAmount(amount, data.data[date][to]));
     } else {
       console.error("Something went wrong. status:  " + request.status);
     }
@@ -21,6 +20,18 @@ function convertCurrency(from, to, amount) {
   request.send();
 }
 
-function calculateAmount(amount, rate){
-    return amount * rate;
+function calculateAmount(amount, rate) {
+  return amount * rate;
+}
+
+function dateString() {
+  var date = new Date();
+
+  var day = String(date.getDate()-1).padStart(2, "0");
+  var month = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var year = date.getFullYear();
+
+  var today = year + "-" + month + "-" + day;
+   
+  return today;
 }
